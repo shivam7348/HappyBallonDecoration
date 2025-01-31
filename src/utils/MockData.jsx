f7950d42b8f966afc512cd6c442d4505b1c68bcd
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-01-31 13:08:09 +0530

    k

--- src/utils/MockData.jsx
+++ src/utils/MockData.jsx
@@ -286,36 +286,35 @@ export const servicescards = [
     id: 1,
     name: "Birthday Decoration",
     image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Birthday-min.jpg",
+      "https://www.funcorp.in/cdn/shop/products/partycorp_happy_birthday_decoration_kit_combo_70_pcs_1__1.jpg?v=1675938604",
   },
   {
     id: 2,
     name: "Wedding Decoration",
     image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Engagement-min.jpg",
+      "https://d397bfy4gvgcdm.cloudfront.net/270602-S-L-New-Jersey-Wedding-1018-1.jpeg",
   },
   {
     id: 3,
     name: "Baby Shower",
-    image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Annaprashan-min.jpg",
+    image: "https://thejarvi.com/wp-content/uploads/2023/01/WEL0002-2.jpg",
   },
   {
     id: 4,
     name: "Corporate Events",
     image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Anniversary-min.jpg",
+      "https://www.hire4event.com/blogs/wp-content/uploads/2023/06/Top-event-venues-in-Delhi-Noida-Gurgaon.jpg",
   },
   {
     id: 5,
     name: "Anniversary Decoration",
     image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Wedding-min.jpg",
+      "https://storage.googleapis.com/shy-pub/337348/1704132265028_SKU-0817_3.jpg",
   },
   {
     id: 6,
     name: "Theme Party",
     image:
-      "https://choudharydecoratorsncr.com/wp-content/uploads/2024/02/Engagement-min.jpg",
+      "https://cdn.webshopapp.com/shops/59317/files/433087475/2000x1500x1/image.jpg",
   },
 ];
