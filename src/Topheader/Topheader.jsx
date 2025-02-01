33c072aace082ac04e1f7045b2dc1ab8fb4ad967
shivam7348 <sachinmax7348@gmail.com>
2025-02-01 11:40:54 +0530

    h

--- src/Topheader/Topheader.jsx
+++ src/Topheader/Topheader.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
+import  { useState, useEffect } from "react";
+import { FaWhatsapp, FaInstagram, FaFacebook, FaPhoneAlt } from "react-icons/fa";
 import Marquee from "react-fast-marquee";
 
 
@@ -21,9 +21,9 @@ const Header = () => {
     <header className="bg-[var(--glsr-red)] text-white p-2">
       <div className="flex flex-col md:flex-row items-center justify-between md:justify-end space-y-2 md:space-y-0 md:space-x-4 md:mr-10">
              <span>
-             <Marquee className="  p-2 text-md">
-            Welcome to Delhi NCR 
-          </Marquee>
+           <Marquee className="p-1 text-xl  font-semibold">
+  Welcome to Delhi NCR!
+</Marquee>
              </span>
         {/* Date and Time */}
         <div className="flex flex-col md:flex-row items-center space-y-1 md:space-y-0 md:space-x-4">
@@ -35,7 +35,7 @@ const Header = () => {
         {/* Company Address */}
         <div className="flex items-center">
           <span className="text-sm md:text-base">
-            123 Company Street, City, Country
+           Ghaziabad
           </span>
         </div>
 
@@ -44,6 +44,7 @@ const Header = () => {
           <FaInstagram className="h-6 w-6 cursor-pointer text-pink-300 hover:text-pink-400" />
           <FaWhatsapp className="h-6 w-6 cursor-pointer text-green-300 hover:text-green-400" />
           <FaFacebook className="h-6 w-6 cursor-pointer text-blue-500 hover:text-blue-400" />
+          <FaPhoneAlt className="h-6 w-6 cursor-pointer text-blue-500 hover:text-blue-400" />
         </div>
       </div>
     </header>
