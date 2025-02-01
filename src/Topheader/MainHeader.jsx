31129dc1961bdac6eab33f8a4b8228713223eb8a
shivam7348 <sachinmax7348@gmail.com>
2025-02-01 12:13:52 +0530

    h

--- src/Topheader/MainHeader.jsx
+++ src/Topheader/MainHeader.jsx
@@ -18,7 +18,7 @@ const MainHeader = () => {
   return (
     <>
       <header className="w-full bg-white shadow-sm sticky top-0 z-50">
-        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="container mx-auto my-auto py-4 flex justify-between items-center">
           <div>
             <img
               src={logo}
@@ -50,85 +50,86 @@ const MainHeader = () => {
 
           {/* Navigation Menu */}
           <nav
-            className={`${isMenuOpen ? "block" : "hidden"
-              } lg:flex lg:items-center lg:space-x-6 absolute lg:static top-16 left-0 w-full lg:w-auto bg-white shadow-lg lg:shadow-none z-50`}
-          >
-            {/* Close Button (visible only on mobile) */}
-            <button
-              onClick={closeMenu}
-              className="lg:hidden absolute top-4 right-4 focus:outline-none"
-              aria-label="Close Menu"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+  className={`${isMenuOpen ? "block" : "hidden"
+    } lg:flex lg:items-center lg:space-x-6 absolute lg:static top-16 left-0 w-full lg:w-auto bg-white shadow-lg lg:shadow-none z-50`}
+>
+  {/* Close Button (Visible only on mobile) */}
+  <button
+    onClick={closeMenu}
+    className="lg:hidden absolute top-4 right-4 focus:outline-none"
+    aria-label="Close Menu"
+  >
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  </button>
 
-            {/* Menu Links */}
-            <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-12 text-gray-800 font-semibold text-xl p-4 lg:p-0 lg:mr-5">
-              <li>
-                <Link
-                  to="/"
-                  className="relative group hover:text-[var(--glsr-red)] block"
-                  onClick={closeMenu}
-                >
-                  Home
-                  <span className="absolute left-0 bottom-0 h-0.5 bg-[var(--glsr-red)] w-0 group-hover:w-full transition-all duration-300"></span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/about"
-                  className="relative group hover:text-[var(--glsr-red)] block"
-                  onClick={closeMenu}
-                >
-                  About
-                  <span className="absolute left-0 bottom-0 h-0.5 bg-[var(--glsr-red)] w-0 group-hover:w-full transition-all duration-300"></span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/services"
-                  className="relative group hover:text-[var(--glsr-red)] block"
-                  onClick={closeMenu}
-                >
-                  Services
-                  <span className="absolute left-0 bottom-0 h-0.5 bg-[var(--glsr-red)] w-0 group-hover:w-full transition-all duration-300"></span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/contact"
-                  className="relative group hover:text-[var(--glsr-red)] block"
-                  onClick={closeMenu}
-                >
-                  Contact
-                  <span className="absolute left-0 bottom-0 h-0.5 bg-[var(--glsr-red)] w-0 group-hover:w-full transition-all duration-300"></span>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/gallery"
-                  className="relative group hover:text-[var(--glsr-red)] block"
-                  onClick={closeMenu}
-                >
-                  Gallery
-                  <span className="absolute left-0 bottom-0 h-0.5 bg-[var(--glsr-red)] w-0 group-hover:w-full transition-all duration-300"></span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+  {/* Menu Links */}
+  <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-12 text-gray-800 font-semibold text-xl p-4 lg:p-0 lg:mr-5">
+    <li>
+      <Link
+        to="/"
+        className="relative group text-gray-600 pb-2 lg:pb-6"
+        onClick={closeMenu}
+      >
+        Home
+        <span className="absolute left-0 bottom-[-6px] lg:bottom-[-4px] w-full h-[3px] bg-pink-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+      </Link>
+    </li>
+    <li>
+      <Link
+        to="/about"
+        className="relative group text-gray-600 pb-2 lg:pb-6"
+        onClick={closeMenu}
+      >
+        About
+        <span className="absolute left-0 bottom-[-6px] lg:bottom-[-4px] w-full h-[3px] bg-pink-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+      </Link>
+    </li>
+    <li>
+      <Link
+        to="/services"
+        className="relative group text-gray-600 pb-2 lg:pb-6"
+        onClick={closeMenu}
+      >
+        Services
+        <span className="absolute left-0 bottom-[-6px] lg:bottom-[-4px] w-full h-[3px] bg-pink-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+      </Link>
+    </li>
+    <li>
+      <Link
+        to="/contact"
+        className="relative group text-gray-600 pb-2 lg:pb-6"
+        onClick={closeMenu}
+      >
+        Contact
+        <span className="absolute left-0 bottom-[-6px] lg:bottom-[-4px] w-full h-[3px] bg-pink-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+      </Link>
+    </li>
+    <li>
+      <Link
+        to="/gallery"
+        className="relative group text-gray-600 pb-2 lg:pb-6"
+        onClick={closeMenu}
+      >
+        Gallery
+        <span className="absolute left-0 bottom-[-6px] lg:bottom-[-4px] w-full h-[3px] bg-pink-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+      </Link>
+    </li>
+  </ul>
+</nav>
+
         </div>
       </header>
     </>
